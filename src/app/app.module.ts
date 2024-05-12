55b263ec92adfc9a400d83ab3a7e9da02ae4b986
Sharik-oss <mamukabigvava2201@gmail.com>
2024-05-12 15:33:10 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MainInformationComponent } from './main-information/main-information.component';
+import { MainHomeComponent } from './main-home/main-home.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainInformationComponent,
+    MainHomeComponent,
+    AboutComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-home/main-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-main-home',
+  templateUrl: './main-home.component.html',
+  styleUrl: './main-home.component.scss'
+})
+export class MainHomeComponent {
+
+}
